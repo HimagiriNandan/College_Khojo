cfed4125571c03622605c9bc69ff4ff531aaeb66
Himagiri Nandan <himagirinandantuduru@gmail.com>
2025-03-08 14:08:51 +0530

    removed public keys

--- server/config/connect.js
+++ server/config/connect.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-// MongoDB connection URI
-const uri = "mongodb+srv://samplesudharsan4:aLIqH9LLjXOwC25F@cluster0.ptyub.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
 async function connectToDatabase() {
   try {
-    await mongoose.connect(uri, {
+    await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -13,7 +10,7 @@ async function connectToDatabase() {
     console.log("Connected to MongoDB successfully using Mongoose!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    process.exit(1); // Exit process if database connection fails
+    process.exit(1);
   }
 }
 
