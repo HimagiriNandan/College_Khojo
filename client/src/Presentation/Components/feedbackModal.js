569783a5344f23fd2f4f8544c27d293639e61945
Shubham <sv9170773305@gmail.com>
2026-09-15 10:34:44 +0530

    Fix: Change name of the Description and remove usename field from feedback section

--- client/src/Presentation/Components/feedbackModal.js
+++ client/src/Presentation/Components/feedbackModal.js
@@ -1,6 +1,5 @@
 //React file imports
 import { useState, useContext } from "react";
-import { useSelector } from "react-redux";
 import { MdCancel } from "react-icons/md";
 
 //component imports
@@ -13,8 +12,6 @@ import { giveFeedback } from "../../Application/Services/api";
 
 
 const FeedbackModal = ({showModal}) => {
-  const email = useSelector((state) => state.user.data.email);
-  const [username, setUsername] = useState("");
   const [description, setDescription] = useState("");
   const [isloading, setIsloading] = useState(false);
   const [stat, setStat] = useState(false);
@@ -30,8 +27,6 @@ const FeedbackModal = ({showModal}) => {
       setIsloading(true);
 
       const res = await giveFeedback({
-        name: username,
-        email: email,
         message: description,
         rating: rating
       });
@@ -63,9 +58,6 @@ const FeedbackModal = ({showModal}) => {
     }
   }
 
-  const handleUsername = (e) => {
-    setUsername(e.target.value);
-  }
 
   const handleDesc = (e) => {
     setDescription(e.target.value);
@@ -87,12 +79,8 @@ const FeedbackModal = ({showModal}) => {
           <div className="cancelIcon" onClick={() => showModal(false)}>
             <MdCancel className="cancelIcon" color="#05B97D" size="1.5rem"/>
           </div>
-          <div style={{marginBottom: "15px"}}>
-            <label className="feedbackmodallabels">Username: </label><br/>
-            <input type="text" placeholder="Enter your name..." className="feedbackmodalinputs" value={username} onChange={handleUsername}></input><br/>
-          </div>
           <div>
-            <label className="feedbackmodallabels">Description: </label><br/>
+            <label className="feedbackmodallabels">Your Review : </label><br/>
             <textarea rows="6" placeholder="Give us your valuable feedbacks" className="feedbackmodalinputs" value={description} onChange={handleDesc}></textarea>
           </div>
           <div className="rating-container">
