42ade396690312ef69adfcbb722086c70f86df8f
Shubham <sv9170773305@gmail.com>
2026-09-17 09:40:00 +0530

    Fix: local url changes

--- client/src/Application/Services/api.js
+++ client/src/Application/Services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // const API = axios.create({ baseURL: "https://khojo-college-server.vercel.app"})
 const API = axios.create({ 
-    baseURL: "http://localhost:8000",
+    baseURL: "https://khojo-college-server.vercel.app",
     withCredentials: true,
 });
 
